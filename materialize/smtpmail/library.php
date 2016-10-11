6b4e59f78195ded2e9475d8a7c4bf18cd29e4521
Bhavesh Patel <bhaveshpatel640@gmail.com>
2016-10-11 12:50:28 +0530

    Update library.php

--- materialize/smtpmail/library.php
+++ materialize/smtpmail/library.php
@@ -2,6 +2,6 @@
 error_reporting(0);
 define("SMTP_HOST", "smtp.gmail.com"); //Hostname of the mail server
 define("SMTP_PORT", "465"); //Port of the SMTP like to be 25, 80, 465 or 587
-define("SMTP_UNAME", "newsplate2015@gmail.com"); //Username for SMTP authentication any valid email created in your domain
-define("SMTP_PWORD", "wtproject"); //Password for SMTP authentication
+define("SMTP_UNAME", "username@gmail.com"); //Username for SMTP authentication any valid email created in your domain
+define("SMTP_PWORD", "password"); //Password for SMTP authentication
 ?>
